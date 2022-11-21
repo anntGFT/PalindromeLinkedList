565bf9d76d6f8a3b9ae9ed3c4d309631ad54cbbb
annt <annt@gft.com>
2022-11-21 10:06:07 +0000

    done with ArrayList

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,42 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.ArrayList;
+
+public class Solution {
+
+    public Solution(){
+
     }
+
+        public boolean isPalindrome(ListNode head){
+
+            ArrayList<Integer> arrayList = new ArrayList<>();
+
+            int pointer = 0;
+
+            while( head != null ){
+
+                pointer++;
+
+                arrayList.add(head.val);
+
+                head = head.next;
+            }
+
+            if( pointer < 1){
+                return true;
+            }
+
+            int pivot = ( pointer - 1) / 2;
+
+            for( int i = 0; i <= pivot; i++){
+
+                if( !arrayList.get(i).equals( arrayList.get(pointer-i-1))){
+
+                    return false;
+                }
+            }
+
+            return true;
+        }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class SolutionTest {
+import org.example.ListNode;
+import org.example.Solution;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SolutionTest {
+
+    Solution solution;
+
+    @BeforeAll
+    void setup(){
+        solution = new Solution();
+    }
+
+    @Test
+    public void linkedListIsPalindrome(){
+        ListNode reverse = new ListNode(4321);
+        ListNode listNode = new ListNode(4321,reverse);
+        assertTrue(solution.isPalindrome(listNode));
+    }
+
+    @Test
+    public void linkedListIsNotPalindrome(){
+        ListNode reverse = new ListNode(2212);
+        ListNode listNode = new ListNode(1234, reverse);
+        assertFalse(solution.isPalindrome(listNode));
+    }
+
 }
